ee1781df782cad3f155672b6dacf22b9e8054c5d
Hernan Faarruggia <hernan.farruggia@gmail.com>
2020-01-20 19:42:18 -0300

    - Added cors and body parser libraries - Updated todos routes and created mock response object

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
     app = express(),
     generalRoutes = require('./routes/general'),
     todosRoutes = require('./routes/todos');
+
+app.use(cors());
+app.use(bodyParser());
 
 generalRoutes.set(app);
 todosRoutes.set(app);
--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,41 +1,46 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Clean the bathroom',
+        state: 'active'
+    },
+    {
+        id: 2,
+        text: 'Do groceries',
+        state: 'active'
+    }
+];
+
 module.exports.set =  (app) => {
     app.get('/todos', (req, res) => {
-        res.send([
-            {
-                id: 1,
-                text: 'tests',
-                state: 'active'
-            },
-            {
-                id: 2,
-                text: 'tests',
-                state: 'active'
-            }
-        ]);
+        res.send(todos);
     });
 
     app.get('/todos/:id', (req, res) => {
-        res.send({
-            id: 1,
-            text: 'tests',
-            state: 'active'
-        });
+        const id = req.params.id;
+ 
+        res.send(todos.find({ id }));
     });
 
     app.post('/todos', (req, res) => {
-        res.send({
-            id: 3,
-            text: 'tests',
-            state: 'active'
-        });
+        const todo = req.body.todo;
+
+        todo.id = todos.length;
+
+        todos.push(todo)
+
+        res.send(todo);
     });
 
     app.put('/todos/:id', (req, res) => {
-        res.send({
-            id: 1,
-            text: 'tests',
-            state: 'active'
-        });
+        const id = req.params.id,
+            newTodo = req.params.todo,
+            oldTodo = todos.find({ id });
+
+        oldTodo.text = newTodo.text;
+        oldTodo.status = newTodo.status;
+
+        res.send(oldTodo);
     });
 
     app.delete('/todos/:id', (req, res) => {
