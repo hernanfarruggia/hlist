914ac909b50dbd28ee2549832940344e56619133
Hernan Faarruggia <hernan.farruggia@gmail.com>
2020-01-22 01:18:14 -0300

    - Updated todos mock - Updated simple response object

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,25 +1,35 @@
 const todos = [
     {
-        id: 1,
+        id: 0,
         text: 'Clean the bathroom',
         state: 'active'
     },
     {
-        id: 2,
+        id: 1,
         text: 'Do groceries',
         state: 'active'
     }
 ];
 
+const response = {
+    data: null,
+    error: null,
+    status: null
+};
+
 module.exports.set =  (app) => {
     app.get('/todos', (req, res) => {
-        res.send(todos);
+        response.data = todos;
+        response.status = 200;
+        res.status(200).send(response);
     });
 
     app.get('/todos/:id', (req, res) => {
         const id = req.params.id;
  
-        res.send(todos.find({ id }));
+        response.data = todos.find({ id });
+        response.status = 200;
+        res.status(200).send(response);
     });
 
     app.post('/todos', (req, res) => {
@@ -29,7 +39,9 @@ module.exports.set =  (app) => {
 
         todos.push(todo)
 
-        res.send(todo);
+        response.data = todo;
+        response.status = 200;
+        res.status(200).send(response);
     });
 
     app.put('/todos/:id', (req, res) => {
@@ -40,10 +52,18 @@ module.exports.set =  (app) => {
         oldTodo.text = newTodo.text;
         oldTodo.status = newTodo.status;
 
-        res.send(oldTodo);
+        response.data = oldTodo;
+        response.status = 200;
+        res.status(200).send(response);
     });
 
     app.delete('/todos/:id', (req, res) => {
-        res.send({})
+        const id = parseInt(req.params.id),
+            todoIndex = todos.findIndex(todo => todo.id === id);
+
+        todos.splice(todoIndex, 1);
+
+        response.status = 204;
+        res.status(204).send(response);
     });
 }
