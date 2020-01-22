b97803f95ad21de3bfece4d4235280c13b1eac15
Hernan Faarruggia <hernan.farruggia@gmail.com>
2020-01-22 01:18:46 -0300

    - Removed unnecesary actions - Added new actions for api failures - Added new error state handling

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,18 +1,23 @@
-export const TODO_ADD = 'TODO_ADD'
 export const TODO_ADD_FAILURE = 'TODO_ADD_FAILURE'
 export const TODO_ADD_SUCCESS = 'TODO_ADD_SUCCESS'
-export const TODO_DELETE = 'TODO_DELETE'
-export const TODO_GET = 'TODO_GET';
+export const TODO_DELETE_FAILURE = 'TODO_DELETE_FAILURE'
+export const TODO_DELETE_SUCCESS = 'TODOS_DELETE_SUCCESS'
 export const TODO_GET_FAILURE = 'TODO_GET_FAILURE';
 export const TODO_GET_SUCCESS = 'TODO_GET_SUCCESS';
-export const TODO_UPDATE = 'TODO_UPDATE'
+export const TODO_UPDATE_SUCCESS = 'TODO_UPDATE_SUCCESS';
+export const TODO_UPDATE_FAILURE = 'TODO_UPDATE_FAILURE';
 
 const url = 'http://localhost:4000/';
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+};
 
 
 export function todos_get () {
     return dispatch => {
         const options = {
+            headers,
             method: 'GET'
         };
 
@@ -43,34 +48,29 @@ function todos_get_failure (err) {
 export function todo_add (text) {
     return dispatch => {
         const options = {
+            headers,
             method: 'POST',
             body: JSON.stringify({
                 todo: {
                     text,
                     status: 'active'
                 }
-            }),
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            }
+            })
         };
         
         fetch(`${url}todos`, options)
             .then(res => {
                 return res.json()
-                    .then(data => data);
+                    .then(res => res.data);
             })
-            .then(todo => {
-                dispatch(todo_add_success(todo))
-            })
+            .then(todo => dispatch(todo_add_success(todo)))
             .catch(err => dispatch(todo_add_failure(err)));
     }
 }
 
 function todo_add_success (todo) {
     return {
-        type: TODO_ADD,
+        type: TODO_ADD_SUCCESS,
         todo
     };
 }
@@ -83,8 +83,28 @@ function todo_add_failure (err) {
 }
 
 export function todo_delete (id) {
+    return dispatch => {
+        const options = {
+            headers,
+            method: 'DELETE'
+        };
+
+        fetch(`${url}todos/${id}`, options)
+            .then(() => dispatch(todos_delete_success(id)))
+            .catch(err => dispatch(todos_delete_failure(err)));
+    }
+}
+
+function todos_delete_success (id) {
     return {
-        type: TODO_DELETE,
+        type: TODO_DELETE_SUCCESS,
         id
+    };
+}
+
+function todos_delete_failure (err) {
+    return {
+        type: TODO_DELETE_FAILURE,
+        msg: err
     };
 }
