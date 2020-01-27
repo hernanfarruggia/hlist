ad4237690073ece522654a663ed5843f11a8c500
Hernan Faarruggia <hernan.farruggia@gmail.com>
2020-01-27 16:30:19 -0300

    - Removed body-parser and replaced with express.json() middleware

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require('express'),
     cors = require('cors'),
-    bodyParser = require('body-parser'),
     app = express(),
     generalRoutes = require('./routes/general'),
     todosRoutes = require('./routes/todos');
 
 app.use(cors());
-app.use(bodyParser());
+app.use(express.json());
 
 generalRoutes.set(app);
 todosRoutes.set(app);
 
-app.listen('4000', () => console.log('Server listening in http:/localhost:4000/'));
+const port = process.env.PORT || 4000; 
+
+app.listen(port, () => console.log(`Server listening in http:/localhost:${port}/`));
