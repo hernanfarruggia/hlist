345581767fc174891b852653b3fb6665313c04ab
Hernan Faarruggia <hernan.farruggia@gmail.com>
2020-01-27 16:31:39 -0300

    - Improved error handling logic in todos api file

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,3 +1,5 @@
+const ERRORS = require('../constants/errors');
+
 const todos = [
     {
         id: 0,
@@ -17,53 +19,82 @@ const response = {
     status: null
 };
 
-module.exports.set =  (app) => {
+module.exports.set = (app) => {
     app.get('/todos', (req, res) => {
         response.data = todos;
         response.status = 200;
-        res.status(200).send(response);
+        res.status(response.status).send(response);
     });
 
     app.get('/todos/:id', (req, res) => {
-        const id = req.params.id;
- 
-        response.data = todos.find({ id });
-        response.status = 200;
-        res.status(200).send(response);
+        if (!req.params.id) {
+            response.error = ERRORS.MISSING_PARAMS;
+            response.status = 400;
+        } else {
+            const todo = todos.find((todo) => todo.id === parseInt(req.params.id));
+            
+            if (!todo) {
+                response.error = ERRORS.NOT_FOUND + ` ID: ${req.params.id}`;
+                response.status = 404;
+            } else {
+                response.data = todo;
+                response.status = 200;
+            }
+        }
+
+        res.status(response.status).send(response);
     });
 
     app.post('/todos', (req, res) => {
-        const todo = req.body.todo;
-
-        todo.id = todos.length;
+        const todo = Object.assign({ id: todos.length }, req.body.todo);
 
         todos.push(todo)
 
         response.data = todo;
         response.status = 200;
-        res.status(200).send(response);
+
+        res.status(response.status).send(response);
     });
 
     app.put('/todos/:id', (req, res) => {
-        const id = req.params.id,
-            newTodo = req.params.todo,
-            oldTodo = todos.find({ id });
+        if (!req.params.id || !req.params.todo) {
+            response.error = ERRORS.MISSING_PARAMS;
+            response.status = 400;
+        } else {
+            const newTodo = req.params.todo,
+                oldTodo = todos.find({ id: req.params.id });
+    
+            if (!oldTodo) {
+                response.error = ERRORS.NOT_FOUND + ` ID: ${req.params.id}`;
+                response.status = 404;
+            } else {
+                oldTodo.text = newTodo.text;
+                oldTodo.status = newTodo.status;
+        
+                response.data = oldTodo;
+                response.status = 200;
+            }
+        }
 
-        oldTodo.text = newTodo.text;
-        oldTodo.status = newTodo.status;
-
-        response.data = oldTodo;
-        response.status = 200;
-        res.status(200).send(response);
+        res.status(response.status).send(response);
     });
 
     app.delete('/todos/:id', (req, res) => {
-        const id = parseInt(req.params.id),
-            todoIndex = todos.findIndex(todo => todo.id === id);
+        if (!req.params.id) {
+            response.error = ERRORS.MISSING_PARAMS;
+            response.status = 400;
+        } else {
+            const todoIndex = todos.findIndex(todo => todo.id === parseInt(req.params.id));
+    
+            if (!todoIndex) {
+                response.error = ERRORS.NOT_FOUND + ` ID: ${req.params.id}`;
+                response.status = 404;
+            } else {
+                todos.splice(todoIndex, 1);
+                response.status = 204;
+            }
+        }
 
-        todos.splice(todoIndex, 1);
-
-        response.status = 204;
-        res.status(204).send(response);
+        res.status(response.status).send(response);
     });
 }
