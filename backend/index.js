a366c787396f905d0c0d08267552eaa9eaee17a4
Hernan Faarruggia <hernan.farruggia@gmail.com>
2020-02-01 03:27:13 -0300

    - Set mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,20 @@ const express = require('express'),
     cors = require('cors'),
     app = express(),
     generalRoutes = require('./routes/general'),
-    todosRoutes = require('./routes/todos');
-
+    todosRoutes = require('./routes/todos'),
+    mongoose = require('mongoose');
+    
 app.use(cors());
 app.use(express.json());
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true })
+    .then(() => console.log('MongoDB initialized successfuly!'))
+    .catch(err => console.log('Error initializing MongoDB:', err));
 
 generalRoutes.set(app);
 todosRoutes.set(app);
